7b27db628393777eab5513296f8414910e5e45d0
Ru2Lu <jiutianlian0@gmail.com>
2023-08-12 15:05:13 +0900

    Initial Commit

---
+++ NotesSwiftUIMVVM/NotesSwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesSwiftUIMVVMApp.swift
+//  NotesSwiftUIMVVM
+//
+//  Created by Ren Usuda on 2023/08/12.
+//
+
+import SwiftUI
+
+@main
+struct NotesSwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
